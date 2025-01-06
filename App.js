fcdab2f437622c14ea73681ce74c5c9f91340969
Aviran485 <aviran05588@gmail.com>
2025-01-06 21:52:14 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import FilterList from "./components/FilterList";
-import WelcomeMessage from "./components/WelcomeMessage";
 import ImageWithCaption from "./components/ImageWithCaption";
 import ColorBlock from "./components/ColorBlock";
 import ActionButton from "./components/ActionButton";
@@ -7,11 +5,6 @@ import ActionButton from "./components/ActionButton";
 
 function App() {
   return (
-    // <div className="App">
-    //   <WelcomeMessage name ='Connor Mcgregor'/>
-    //   <FilterList words = {['react','jsx','javascript','ui']} minLength={5}/>
-    //   <FilterList/>
-    // </div>
     <div>
       {/* Exercise 1 */}
       <h3>ColorBlock Examples:</h3>
